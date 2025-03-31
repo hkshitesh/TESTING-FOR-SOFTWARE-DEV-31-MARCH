0cea24154da17036dcd05824eaff738996df9f86
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2025-03-31 21:22:33 +0530

    Create CheckoutSystem.java

---
+++ DAY-1/CODE/CheckoutSystem.java
@@ -0,0 +1,29 @@
+public class CheckoutSystem {
+	
+	public static String processOrder(double price, int quantity)
+	{
+        if (quantity <= 0) {
+            return "Invalid Order";
+        }
+        
+        double total = price* quantity;
+        
+        // Apply discount if total > $100
+        if (total >= 100) {
+            total *= 0.9;
+        }
+     // Add shipping fee if total < $10
+        if (total <= 10) {
+            total += 2;
+        }
+        return "Total Payable: $" + String.format("%.2f", total);
+
+	}		
+
+	public static void main(String[] args) {
+
+		System.out.println(processOrder(1, 5));
+
+	}
+
+}
